0c30a1136e9911ecd433dc1e90049633fb814fab
saepulfariz <saepulfariz108@gmail.com>
2025-12-28 12:58:16 +0700

    add function temporary language

--- app/Helpers/custom_helper.php
+++ app/Helpers/custom_helper.php
@@ -5,3 +5,18 @@ function asset_url($path = '')
 {
     return base_url(PUBLIC_PATH . $path);
 }
+
+
+function temp_lang(string $line, array $args = [], ?string $locale = null)
+{
+    $cache = \Config\Services::cache();
+    $cacheKey = $line;
+    if (!$cache->get($cacheKey)) {
+        $data = lang($line, $args, $locale);
+        $cache->save('lang_' . $cacheKey . '_' . service('request')->getLocale(), $data, CACHE_TTL); // Cache for 60 minutes
+    } else {
+        $data = $cache->get($cacheKey);
+    }
+
+    return $data;
+}
--- app/Views/template/topbar.php
+++ app/Views/template/topbar.php
@@ -89,8 +89,8 @@
         </li>
         <!-- Menu Footer-->
         <li class="user-footer">
-          <a href="<?= base_url('change_password'); ?>" class="btn btn-default btn-flat"><?= lang('app.change-password') ?></a>
-          <a href="<?= base_url('logout'); ?>" class="btn btn-default btn-flat float-right"><?= lang('app.logout') ?></a>
+          <a href="<?= base_url('change_password'); ?>" class="btn btn-default btn-flat"><?= temp_lang('app.change-password') ?></a>
+          <a href="<?= base_url('logout'); ?>" class="btn btn-default btn-flat float-right"><?= temp_lang('app.logout') ?></a>
         </li>
       </ul>
     </li>
